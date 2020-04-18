0897db64d40aaf8abce19d13663ce8a9ecb5805e
Rildo <odlirgz@gmail.com>
2020-04-17 19:00:28 -0500

    pull

--- front-end/src/app/components/empresas/crud-empresa/crud-empresa.component.ts
+++ front-end/src/app/components/empresas/crud-empresa/crud-empresa.component.ts
@@ -1,15 +1,15 @@
-import { MatStepper } from '@angular/material/stepper';
-import { HttpParams } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { TokenService } from '../../../Services/token/token.service';
-import { ApiBackRequestService } from './../../../Services/api-back-request.service';
-import { Component, OnInit, ViewChild} from '@angular/core';
+import {MatStepper} from '@angular/material/stepper';
+import {HttpParams} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {TokenService} from '../../../Services/token/token.service';
+import {ApiBackRequestService} from './../../../Services/api-back-request.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-crud-empresa',
-  templateUrl: './crud-empresa.component.html',
-  styleUrls: ['./crud-empresa.component.css']
+	selector: 'app-crud-empresa',
+	templateUrl: './crud-empresa.component.html',
+	styleUrls: ['./crud-empresa.component.css']
 })
 export class CrudEmpresaComponent implements OnInit {
 
@@ -29,103 +29,89 @@ export class CrudEmpresaComponent implements OnInit {
 		edit: {name: ''},
 		created_at: null,
 		updated_at: null,
-		sucursales : []
+		sucursales: []
 	};
 
-	public titulo= "CREAR EMPRESA";
+	public titulo = 'CREAR EMPRESA';
 
-  	public id: HttpParams;
+	public id: HttpParams;
 
-  constructor(
-    private api: ApiBackRequestService,
-    private user: TokenService,
-	private activatedRoute: ActivatedRoute,
-	private router: Router
+	constructor(
+		private api: ApiBackRequestService,
+		private user: TokenService,
+		private activatedRoute: ActivatedRoute,
+		private router: Router
 	) {
-
 	}
 
-  ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(async params => {
-		this.id = params.id;
-		let tab= params.tab;
-        if (this.id != null) {
-			if(tab!=null)
-			{
-				this.cargarEditar(1);
+	ngOnInit(): void {
+		this.activatedRoute.queryParams.subscribe(async params => {
+			this.id = params.id;
+			let tab = params.tab;
+			if (this.id != null) {
+				if (tab != null) {
+					this.cargarEditar(1);
+				} else {
+					this.cargarEditar();
+				}
 			}
-			else
-			{
-				this.cargarEditar();
-			}
-		}
-	});
+		});
 	}
 
-  async cargarEditar(next?)
-  {
-		this.titulo = "EDITAR EMPRESA";
-    await this.api.show('empresas', this.id).toPromise().then(
-      (data) => {
-			this.form = data;
-			this.stepper.selected.completed = true;
-			if(next)
-			{
-				this.stepper.next();
+	async cargarEditar(next?) {
+		this.titulo = 'EDITAR EMPRESA';
+		await this.api.show('empresas', this.id).toPromise().then(
+			(data) => {
+				this.form = data;
+				this.stepper.selected.completed = true;
+				if (next) {
+					this.stepper.next();
+				}
 			}
+		);
+	}
+
+	guardar() {
+		if (this.id) {
+			this.editar();
+		} else {
+			this.registrar();
 		}
-    );
-  }
+	}
 
-  guardar()
-  {
-    if(this.id)
-    {
-      this.editar();
-    }
-    else
-    {
-      this.registrar();
-    }
-  }
+	return() {
+		this.router.navigateByUrl('/empresas');
+	}
 
-  return()
-  {
-	this.router.navigateByUrl('/empresas');
-  }
-
-  async registrar()
-  {
-    await this.api.post('empresas', this.form).toPromise().then(
+	async registrar() {
+		await this.api.post('empresas', this.form).toPromise().then(
 			(data) => {
 				this.handleRegistrar(data);
 				this.return();
 			}
-    );
+		);
 	}
 
-	handleRegistrar(data)
-	{
-		this.id= data.id;
-		this.form=data;
+	handleRegistrar(data) {
+		this.id = data.id;
+		this.form = data;
 		this.stepper.selected.completed = true;
 		this.stepper.next();
-		this.titulo = "EDITAR EMPRESA";
+		this.titulo = 'EDITAR EMPRESA';
 	}
 
-	handleEditar(data)
-	{
-		this.form=data;
+	handleEditar(data) {
+		this.form = data;
 	}
 
-  async editar()
-  {	this.form.edit_user_id = this.user.me();
-    await this.api.put('empresas', this.id , this.form).toPromise().then(
-      (data) => {
-		  this.handleEditar(data);
-		  }
-    );
-  }
+	async editar() {
+		this.form.edit_user_id = this.user.me();
+		await this.api.put('empresas', this.id, this.form).toPromise().then(
+			(data) => {
+				this.handleEditar(data);
+			}
+		);
+	}
 
 }
 
--- front-end/src/app/components/test-interes/test-interes.component.ts
+++ front-end/src/app/components/test-interes/test-interes.component.ts
@@ -1,11 +1,11 @@
-import { HttpParams } from '@angular/common/http';
-import { ApiBackRequestService } from './../../Services/api-back-request.service';
-import { Component, OnInit } from '@angular/core';
+import {HttpParams} from '@angular/common/http';
+import {ApiBackRequestService} from './../../Services/api-back-request.service';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-test-interes',
-  templateUrl: './test-interes.component.html',
-  styleUrls: ['./test-interes.component.css']
+	selector: 'app-test-interes',
+	templateUrl: './test-interes.component.html',
+	styleUrls: ['./test-interes.component.css']
 })
 export class TestInteresComponent implements OnInit {
 
@@ -18,64 +18,65 @@ export class TestInteresComponent implements OnInit {
 	encuestas = [];
 
 	subencuesta = {
-		pregunta_id : null,
-		subpregunta_id :null,
-		respuesta_id :null
+		pregunta_id: null,
+		subpregunta_id: null,
+		respuesta_id: null
+	};
+
+	subencuestas = [];
+
+	constructor(private api: ApiBackRequestService) {
 	}
 
-	subencuestas=[
-
-	]
-
-  constructor(private api: ApiBackRequestService) { }
-
-  ngOnInit(): void {
+	ngOnInit(): void {
 
 		this.fetch();
 	}
 
-	async fetch()
-	{
+	async fetch() {
 		await this.api.show('preguntas', 1).toPromise().then(
-      (data) => {this.preguntas=data}
+			(data) => {
+				this.preguntas = data;
+			}
 		);
 
 		await this.api.show('subpreguntas', 1).toPromise().then(
-      (data) => {this.subpreguntas=data;}
+			(data) => {
+				this.subpreguntas = data;
+			}
 		);
 
 		await this.api.show('respuestas', 1).toPromise().then(
-      (data) => {this.respuestas=data;}
-    );
+			(data) => {
+				this.respuestas = data;
+			}
+		);
 	}
 
-	guardar()
-	{
+	guardar() {
 		console.log(this.encuestas);
 	}
 
-	select(index_preg,index_sub,pregunta_id,subpregunta_id,respuesta_id)
-	{
-		this.subencuesta.pregunta_id= pregunta_id;
-		this.subencuesta.subpregunta_id= subpregunta_id;
-		this.subencuesta.respuesta_id= respuesta_id;
+	select(index_preg, index_sub, pregunta_id, subpregunta_id, respuesta_id) {
+		this.subencuesta.pregunta_id = pregunta_id;
+		this.subencuesta.subpregunta_id = subpregunta_id;
+		this.subencuesta.respuesta_id = respuesta_id;
 
-		this.subencuestas[index_sub]= JSON.parse(JSON.stringify(this.subencuesta));
+		this.subencuestas[index_sub] = JSON.parse(JSON.stringify(this.subencuesta));
 
-		this.encuestas[index_preg]= JSON.parse(JSON.stringify( this.subencuestas));
+		this.encuestas[index_preg] = JSON.parse(JSON.stringify(this.subencuestas));
 
 		this.limpiar();
 
 		console.log(this.encuestas);
 	}
 
-	limpiar()
-	{
+	limpiar() {
 		this.subencuesta = {
-			pregunta_id : null,
-			subpregunta_id :null,
-			respuesta_id :null
-		}
+			pregunta_id: null,
+			subpregunta_id: null,
+			respuesta_id: null
+		};
 	}
 
 }
