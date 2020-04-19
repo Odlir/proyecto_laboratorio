2edef098c3e9d3e1fe15c36aa3f6615cbdc7a35a
Rildo <odlirgz@gmail.com>
2020-04-19 14:25:18 -0500

    reactive form

--- front-end/src/app/components/test-interes/test-interes.component.ts
+++ front-end/src/app/components/test-interes/test-interes.component.ts
@@ -1,13 +1,12 @@
-
-import { HttpParams } from '@angular/common/http';
-import { ApiBackRequestService } from './../../Services/api-back-request.service';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms'
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {ApiBackRequestService} from './../../Services/api-back-request.service';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-test-interes',
-  templateUrl: './test-interes.component.html',
-  styleUrls: ['./test-interes.component.css']
+	selector: 'app-test-interes',
+	templateUrl: './test-interes.component.html',
+	styleUrls: ['./test-interes.component.css']
 })
 export class TestInteresComponent implements OnInit {
 
@@ -21,88 +20,94 @@ export class TestInteresComponent implements OnInit {
 		respuesta_id: null,
 		encuesta_id: null,
 		persona_id: null
+	};
+
+	respuestas1 = [];
+	respuestas2 = [];
+	data = [];
+	formGroup: FormGroup;
+	group: any = {};
+
+	constructor(private api: ApiBackRequestService, public formBuilder: FormBuilder) {
+		this.formGroup = new FormGroup(this.group);
 	}
 
-	respuestas1= [];
-
-	respuestas2= [];
-
-	data = [];
-
-	FormGroup: FormGroup;
-
-  constructor(private api: ApiBackRequestService, public formBuilder: FormBuilder) {
-
-  }
-
-  ngOnInit(): void {
+	ngOnInit(): void {
 
 		this.fetch();
 	}
 
-	reactive(preguntas)
-	{
-		this.FormGroup = this.formBuilder.group({
-			preguntas: ['']
-		  });
-
-			const preguntasMethodsControl = <FormArray>this.FormGroup.controls['productMethod'];
-			// creating radio button control for each item.
-			for (let i = 0 ; i < preguntas.length; i++) {
-			preguntasMethodsControl.push(new FormControl());
-			}
-
-			console.log(this.FormGroup);
+	reactive(preguntas) {
+		preguntas.forEach((pr, index) => {
+			pr.subpreguntas.forEach( (p, i) => {
+				let ind = `${index}-${i}`;
+				this.group[ind] = new FormControl('', [Validators.required])
+			})
+		});
 	}
 
-	async fetch()
-	{
+	async fetch() {
 		await this.api.show('preguntas', 1).toPromise().then(
-      (data) => {this.preguntas=data}
+			(data) => {
+				this.preguntas = data;
+			}
 		);
 
 		await this.api.show('subpreguntas', 1).toPromise().then(
-      (data) => {this.subpreguntas=data;}
+			(data) => {
+				this.subpreguntas = data;
+			}
 		);
 
 		await this.api.get('respuestas?encuesta=1&sub=1').toPromise().then(
-			(data) => {this.respuestas1=data;}
+			(data) => {
+				this.respuestas1 = data;
+			}
 		);
 
 		await this.api.get('respuestas?encuesta=1&sub=2').toPromise().then(
-			(data) => {this.respuestas2=data;}
+			(data) => {
+				this.respuestas2 = data;
+			}
 		);
 
 		this.subpreguntas.forEach(element => {
-			if(element.tipo_subpregunta=='1')
-			{
-				element.respuestas= JSON.parse(JSON.stringify(this.respuestas1));
+			if (element.tipo_subpregunta == '1') {
+				element.respuestas = JSON.parse(JSON.stringify(this.respuestas1));
+			} else {
+				element.respuestas = JSON.parse(JSON.stringify(this.respuestas2));
 			}
-			else
-			{
-				element.respuestas= JSON.parse(JSON.stringify(this.respuestas2));
-			}
-			element.respuesta_id= null;
+			element.respuesta_id = null;
 		});
 
 		this.preguntas.forEach(element => {
-			element.subpreguntas= JSON.parse(JSON.stringify(this.subpreguntas));
+			element.subpreguntas = JSON.parse(JSON.stringify(this.subpreguntas));
 		});
 
 		this.reactive(this.preguntas);
 	}
 
-	async guardar()
-	{
-		this.preguntas.forEach(preg => {
+	async guardar() {
+		console.log('form', this.formGroup);
+		Object.values(this.formGroup.controls).forEach( a => {
+			//console.log('a', a.status);
+			if (a.status == 'INVALID'){
+				Swal.fire('Hello world!');
+			}
+		});
+
+		return false;
+
+		//this.formGroup.controls
+		/*this.preguntas.forEach(preg => {
 			preg.subpreguntas.forEach(async sub => {
-				this.form.pregunta_id= preg.id;
-				this.form.subpregunta_id= sub.id;
-				this.form.respuesta_id= sub.respuesta_id;
+				this.form.pregunta_id = preg.id;
+				this.form.subpregunta_id = sub.id;
+				this.form.respuesta_id = sub.respuesta_id;
 
 				this.data.push(this.form);
 
-				this.limpiar()
+				this.limpiar();
 			});
 		});
 
@@ -110,17 +115,16 @@ export class TestInteresComponent implements OnInit {
 			(data) => {
 
 			}
-		  );
+		);*/
 	}
 
-	limpiar()
-	{
+	limpiar() {
 		this.form = {
 			pregunta_id: null,
 			subpregunta_id: null,
 			respuesta_id: null,
 			encuesta_id: null,
 			persona_id: null
-		}
+		};
 	}
 }
